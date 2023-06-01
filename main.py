52dac649d9113ad7505d1a641740ff29b364ebfd
Gratik1 <103760215+Gratik1@users.noreply.github.com>
2023-06-01 19:41:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
+import shutil
 
+from_dir = "c:/users/stanz/downloads"
+
+list = os.listdir(from_dir)
+print(list)
+
+for file in list :
+    name,ext = os.path.splitext(file)
+    print(name)
+    print(ext)
